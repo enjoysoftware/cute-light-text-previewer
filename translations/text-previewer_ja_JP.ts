352e60f39deca0785ca16b50521a3bb398b76a50
enjoysoftware <username@users.noreply.github.com>
2019-12-11 17:23:20 +0900

    Create Japanese translation

---
+++ translations/text-previewer_ja_JP.ts
@@ -0,0 +1,32 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ja_JP">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../viewer_mainwin.ui" line="14"/>
+        <source>Cute Light Text Previewer</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../viewer_mainwin.ui" line="40"/>
+        <source>toolBar</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../viewer_mainwin.ui" line="55"/>
+        <source>&amp;Open</source>
+        <translation>開く(&amp;O)</translation>
+    </message>
+    <message>
+        <location filename="../viewer_mainwin.ui" line="58"/>
+        <source>Open</source>
+        <translation>開く</translation>
+    </message>
+    <message>
+        <location filename="../viewer_mainwin.ui" line="61"/>
+        <source>O</source>
+        <translation>O</translation>
+    </message>
+</context>
+</TS>
